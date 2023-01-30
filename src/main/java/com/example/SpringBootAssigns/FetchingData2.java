bee3703d56e1e59811cd29434e0caa600f8554c3
Reena Kahar <reenabadera8999@gmail.com>
2023-01-30 23:05:15 +0530

    Fetching Data 2

--- src/main/java/com/example/SpringBootAssigns/FetchingData2.java
+++ src/main/java/com/example/SpringBootAssigns/FetchingData2.java
@@ -5,21 +5,18 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
-public class FetchingData1 {
-    public static void main(String[] args) {
-        try {
-            FetchdatawithApi();
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
+public class FetchingData2 {
+    public static void main(String[] args) throws Exception {
+        FetchdatawithApi2();
     }
-    public static void FetchdatawithApi() throws Exception {
-        String url = "https://api.chucknorris.io/";
+    public static void FetchdatawithApi2() throws Exception {
+        String url = "https://www.weatherbit.io/";
 
         HttpRequest request = HttpRequest.newBuilder().GET().uri(URI.create(url)).build();
         HttpClient httpClient = HttpClient.newBuilder().build();
         HttpResponse<String> httpResponse = httpClient.send(request,HttpResponse.BodyHandlers.ofString());
-        System.out.println(httpResponse.statusCode());
+//        System.out.println(httpResponse.statusCode());
         System.out.println(httpResponse.body());
     }
+
 }
